91960b5cdc87e073727ccd5253516992a2dafc1e
Hisham Dulfacker <dulfackerhisham@gmail.com>
2025-06-21 12:22:04 +0530

    pyramid star own logic

--- 01 - pattern/star pyramid/sp.py
+++ 01 - pattern/star pyramid/sp.py
@@ -6,4 +6,15 @@ for i in range(n):
 
     for j in range(2*i+1):
         print("*", end="")
+
+    for j in range(n-i-1):
+        print(" ", end="")
     print()
+
+#Pyramid
+# n = 6
+# for i in range(1, n+1):
+#     for j in range(1, n - i +1):  #nth(n=6) iteration is happening which is not needed thats the reason we are getting at last a empty line
+#         print(" ", end=" ")
+#         if j == (n - i):
+#             print("* " * (2*i-1))
